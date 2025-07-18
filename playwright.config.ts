05a8629b4a142fceee92661be4da0ab951bdb648
Uzair Khan <khanuzair54670553@agentforce.com>
2025-07-18 20:02:21 +0530

    HTML Reporter Config Changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', {open: 'always'}]],
+  reporter: [['html', {open: 'never'}]],
   timeout: 90000,
   expect: {
     timeout: 30000,
